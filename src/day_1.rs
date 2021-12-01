39963413fa0e33d895726c2c567465ce62930992
ajgoodm <aarongoodm@gmail.com>
2021-12-01 14:41:52 -0500

    part 2

--- src/day_1.rs
+++ src/day_1.rs
@@ -3,11 +3,15 @@ pub mod day_1 {
     use crate::input::read_input::AocBufReader;
 
 
-    pub fn part_1(aoc_reader: AocBufReader) -> usize {
-        let inputs: Vec<usize> = aoc_reader.map(
+    fn read_input(aoc_reader: AocBufReader) -> Vec<usize> {
+        aoc_reader.map(
             |line| {line.parse::<usize>().unwrap()}
-        ).collect();
+        ).collect::<Vec<usize>>()
+    }
 
+
+    pub fn part_1(aoc_reader: AocBufReader) -> usize {
+        let inputs = read_input(aoc_reader);
         Zip::new((&inputs, &inputs[1..])).map(
             |(element, next_element)| {
                 match next_element > element {
@@ -16,4 +20,17 @@ pub mod day_1 {
                 }
             }).sum()
     }
+
+
+    pub fn part_2(aoc_reader: AocBufReader) -> usize {
+        let inputs = read_input(aoc_reader);
+        Zip::new((&inputs, &inputs[1..], &inputs[2..], &inputs[3..])).map(
+            |(first, second, third, fourth)| {
+                match (second + third + fourth) > (first + second + third) {
+                    true => 1,
+                    false => 0
+                }
+            }).sum()
+    }
+
 }
