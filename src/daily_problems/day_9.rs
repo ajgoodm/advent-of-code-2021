a1957706d056d5aa4f5adc9e0852a946bdbad3f1
ajgoodm <aarongoodm@gmail.com>
2021-12-09 15:56:53 -0500

    day 9

--- src/daily_problems/day_9.rs
+++ src/daily_problems/day_9.rs
@@ -42,29 +42,65 @@ pub mod solutions {
             Some(&self.depths[row_idx][col_idx])
         }
 
+        fn get_adjacent(&self, row_idx: usize, col_idx: usize) -> Vec<((usize, usize), &usize)> {
+            let mut adjacent_pts: Vec<((usize, usize), &usize)> = vec![];
+            if col_idx > 0 {
+                if let Some(north_neighbor) = self.get_depth(row_idx, col_idx - 1) {
+                    adjacent_pts.push(((row_idx, col_idx - 1), north_neighbor));
+                }
+            }
+            if let Some(south_neighbor) = self.get_depth(row_idx, col_idx + 1) {
+                adjacent_pts.push(((row_idx, col_idx + 1), south_neighbor));
+            }
+            if row_idx > 0 {
+                if let Some(west_neighbor) = self.get_depth(row_idx - 1, col_idx) {
+                    adjacent_pts.push(((row_idx - 1, col_idx), west_neighbor));
+                }
+            }
+            if let Some(east_neighbor) = self.get_depth(row_idx + 1, col_idx) {
+                adjacent_pts.push(((row_idx + 1, col_idx), east_neighbor));
+            }
+
+            adjacent_pts
+        }
+
         fn local_minima(&self) -> Vec<((usize, usize), usize)> {
             let mut minima: Vec<((usize, usize), usize)> = vec![];
+            let mut is_minimum: bool;
             for ((row_idx, col_idx), depth) in self.iter_depths() {
-                if col_idx > 0 {
-                    if let Some(north_neighbor) = self.get_depth(row_idx, col_idx - 1) {
-                       if north_neighbor <= depth {continue}
+                is_minimum = true;
+                for (_, adjacent_depth) in self.get_adjacent(row_idx, col_idx) {
+                    if adjacent_depth <= depth {
+                        is_minimum = false;
+                    }
                 }
+                if is_minimum {
+                    minima.push(((row_idx, col_idx), *depth));
                 }
-                if let Some(south_neighbor) = self.get_depth(row_idx, col_idx + 1) {
-                    if south_neighbor <= depth {continue}
-                }
-                if row_idx > 0 {
-                    if let Some(west_neighbor) = self.get_depth(row_idx - 1, col_idx) {
-                        if west_neighbor <= depth {continue}
-                }
-                }
-                if let Some(east_neighbor) = self.get_depth(row_idx + 1, col_idx) {
-                    if east_neighbor <= depth {continue}
-                }
-                minima.push(((row_idx, col_idx), *depth));
             }
 
             minima
+        }
+
+        fn basin_size(&self, mut row_idx: usize, mut col_idx: usize) -> usize {
+            let mut locations_to_explore: Vec<(usize, usize)> = vec![(row_idx, col_idx)];
+            let mut explored_locations: HashSet<(usize, usize)> = vec![(row_idx, col_idx)].into_iter().collect();
+            let mut locations_in_basin: HashSet<(usize, usize)> = HashSet::new();
+
+            while locations_to_explore.len() > 0 {
+                let (row_idx, col_idx) = locations_to_explore.pop().unwrap();
+                locations_in_basin.insert((row_idx, col_idx));
+
+                let current_depth = self.get_depth(row_idx, col_idx).unwrap();
+                for ((next_row_idx, next_col_idx), next_depth) in self.get_adjacent(row_idx, col_idx) {
+                    explored_locations.insert((next_row_idx, next_col_idx));
+                    if *next_depth != 9usize && next_depth > current_depth {
+                        locations_to_explore.push((next_row_idx, next_col_idx));
+                    }
+                }
+            }
+
+            locations_in_basin.len()
         }
     }
 
@@ -72,5 +108,15 @@ pub mod solutions {
     pub fn part_1(aoc_reader: AocBufReader) -> usize {
         let sea_floor_map = SeaFloorMap::from_aoc_reader(aoc_reader);
         sea_floor_map.local_minima().iter().map(|((_, _), depth)| depth + 1).sum()
+    }
+
+
+    pub fn part_2(aoc_reader: AocBufReader) -> usize {
+        let sea_floor_map = SeaFloorMap::from_aoc_reader(aoc_reader);
+        let mut basin_sizes: Vec<usize> = sea_floor_map.local_minima().iter()
+            .map(|((row_idx, col_idx), _)| sea_floor_map.basin_size(*row_idx, *col_idx))
+            .collect();
+        basin_sizes.sort_by(|a, b| b.cmp(a));
+        basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
     }
 }
