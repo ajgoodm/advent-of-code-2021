c6f662dd9042f28cacda4b6d444779e26a4a8d73
ajgoodm <aarongoodm@gmail.com>
2021-12-12 12:07:14 -0500

    day 12 part 1

---
+++ src/daily_problems/day_12.rs
@@ -0,0 +1,100 @@
+pub mod solutions {
+    use std::collections::{HashMap, HashSet};
+
+    use lazy_static::lazy_static;
+
+    use crate::AocBufReader;
+    use crate::utils::str_utils::is_lower_case;
+
+    lazy_static! {
+        static ref START: String  = "start".to_string();
+        static ref END: String = "end".to_string();
+    }
+
+
+    #[derive(Clone)]
+    struct Path {
+        nodes: Vec<String>
+    }
+
+
+    impl Path {
+        fn new(node: &String) -> Path {
+            let mut path = Path { nodes: vec![] };
+            path.add_node(node);
+            path            
+        }
+
+        fn add_node(&mut self, node: &String) {
+            self.nodes.push(node.clone());
+        }
+
+        fn terminus(&self) -> &String {
+            self.nodes.last().unwrap()
+        }
+
+        fn was_small_cave_visited(&self, node: &String) -> bool {
+            if !is_lower_case(node) {
+                false
+            } else {
+                self.nodes.contains(node)
+            }
+        }
+    }
+
+
+    struct CaveMap {
+        graph: HashMap<String, HashSet<String>>
+    }
+
+
+    impl CaveMap {
+        fn from_reader(aoc_reader: AocBufReader) -> CaveMap {
+            let mut graph: HashMap<String, HashSet<String>> = HashMap::new();
+            for line in aoc_reader {
+                let nodes: Vec<&str> = line.split("-").collect();
+                assert_eq!(nodes.len(), 2);
+
+                graph.entry(nodes[0].to_string()).or_insert(HashSet::new()).insert(nodes[1].to_string());
+                graph.entry(nodes[1].to_string()).or_insert(HashSet::new()).insert(nodes[0].to_string());
+            }
+            CaveMap { graph: graph }
+        }
+
+        fn get_adjacent_nodes(&self, node: &String) -> Vec<&String> {
+            if let Some(neighbors) = self.graph.get(node) {
+                return neighbors.iter().collect()
+            } else {return vec![]}
+        }
+
+        fn find_paths(&self) -> Vec<Path> {
+            let mut living_paths: Vec<Path> = vec![Path::new(&START)];
+            let mut complete_paths: Vec<Path> = vec![];
+            while living_paths.len() > 0 {
+                let this_path = living_paths.pop().unwrap();
+                for next_node in self.get_adjacent_nodes(this_path.terminus()) {
+                    if !this_path.was_small_cave_visited(next_node) {
+                        if &next_node[..] == &END[..] {
+                            let mut finished_path = this_path.clone();
+                            finished_path.add_node(&END);
+                            complete_paths.push(finished_path);
+                        } else {
+                            let mut living_path = this_path.clone();
+                            living_path.add_node(next_node);
+                            living_paths.push(living_path);
+                        }
+                    }
+                }
+
+            }
+            complete_paths
+        }
+    }
+
+
+    pub fn part_1(aoc_reader: AocBufReader) -> usize {
+        let cave_map = CaveMap::from_reader(aoc_reader);
+        cave_map.find_paths().len()
+    }
+
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,11 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref LOWER_CASE_RE: Regex = Regex::new(r"^[a-z]*$").unwrap();
+}
+
+
 pub mod conversion {
     pub fn binary_string_to_usize(binary: String) -> usize {
         binary.chars().rev().enumerate().map(|(idx, c)| {
@@ -12,8 +20,34 @@ pub mod conversion {
 }
 
 
+pub mod str_utils {
+    use super::LOWER_CASE_RE;
+
+    pub fn is_lower_case(string: &String) -> bool {
+        match LOWER_CASE_RE.find(string) {
+            Some(_) => true,
+            None => false
+        }
+    }
+}
+
+
 pub mod stats {
     pub fn f64_avg_usize(vals: &Vec<usize>) -> f64 {
         vals.iter().sum::<usize>() as f64 / vals.len() as f64
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::str_utils::*;
+
+    #[test]
+    fn test_is_lower() {
+        assert_eq!(is_lower_case(&"A".to_string()), false);
+        assert_eq!(is_lower_case(&"".to_string()), true);
+        assert_eq!(is_lower_case(&"Aa".to_string()), false);
+        assert_eq!(is_lower_case(&"abc".to_string()), true);
     }
 }
