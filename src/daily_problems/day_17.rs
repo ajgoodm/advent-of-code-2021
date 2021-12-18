1be3a5c79630c0a11971ff866332e16d4b385eb2
ajgoodm <aarongoodm@gmail.com>
2021-12-17 22:05:23 -0500

    day 17 pt 2

--- src/daily_problems/day_17.rs
+++ src/daily_problems/day_17.rs
@@ -1,4 +1,6 @@
 pub mod solutions {
+    use itertools::Itertools;
+
     use crate::AocBufReader;
 
     use lazy_static::lazy_static;
@@ -60,6 +62,14 @@ pub mod solutions {
             self.vy -= 1;
         }
 
+        fn hits(&mut self, target: &Target) -> bool {
+            loop {
+                if self.sy < target.min_y || self.sx > target.max_x { return false }
+                if target.contains_probe(&self) { return true }
+                self.step()
+            }
+        }
+
         fn print(&self) {
             println!("({}, {})", self.sx, self.sy);
         }
@@ -76,17 +86,43 @@ pub mod solutions {
 
     }
 
+
+    fn get_min_vx(target: &Target) -> isize {
+        (-0.5 + (0.25 + 2.0 * target.min_x as f64).powf(0.5)).floor() as isize
+    }
+
+
     fn _part_1(input: String) -> usize {
         let target = read_input(input);
+        let mut max_vy: isize = isize::MIN;
+        for (vx_initial, vy_initial) in (get_min_vx(&target)..target.max_x + 1).cartesian_product(0..target.min_y.abs()) {
+            if vy_initial > max_vy && Probe::new(vx_initial, vy_initial).hits(&target) {
+                max_vy = vy_initial;
+            }
 
+        }
 
-        1
+        ((max_vy + 1) * max_vy / 2) as usize
+    }
+
+    fn _part_2(input: String) -> usize {
+        let target = read_input(input);
+        let mut n_valid_paths: usize = 0;
+        for (vx_initial, vy_initial) in (get_min_vx(&target)..target.max_x + 1).cartesian_product(-target.min_y.abs()..target.min_y.abs()) {
+            if Probe::new(vx_initial, vy_initial).hits(&target) {
+                n_valid_paths += 1
+            }
+        }
+        n_valid_paths
     }
 
     pub fn part_1(mut aoc_reader: AocBufReader) -> usize {
         _part_1(aoc_reader.next().unwrap())
     }
 
+    pub fn part_2(mut aoc_reader: AocBufReader) -> usize {
+        _part_2(aoc_reader.next().unwrap())
+    }
 
     #[cfg(test)]
     mod tests {
@@ -108,7 +144,13 @@ pub mod solutions {
 
         #[test]
         fn test_pt_1() {
-            _part_1("target area: x=20..30, y=-10..-5".to_string());
+            // println!("{}", _part_1("target area: x=155..182, y=-117..-67".to_string()));
+        }
+
+
+        #[test]
+        fn test_pt_2() {
+            println!("{}", _part_2("target area: x=155..182, y=-117..-67".to_string()));
         }
     }
 }
